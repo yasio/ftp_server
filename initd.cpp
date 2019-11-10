32c77e2e8555740a29b954addc675b0e2e99b737
halx99 <halx99@live.com>
2019-11-10 20:21:07 +0800

    daemon support

---
+++ initd.cpp
@@ -0,0 +1,60 @@
+#ifdef __linux
+
+#  include <iostream>
+#  include <unistd.h>
+#  include <signal.h>
+#  include <sys/param.h>
+#  include <sys/types.h>
+#  include <sys/stat.h>
+
+void sighandler(int signum)
+{
+  if (SIGUSR1 == signum)
+  {
+    std::cout << "catched external stop signal, now stop the server...\n";
+
+    std::cout << "post stop signals to server sucessfully.\n";
+  }
+}
+
+void sinitd(void)
+{
+  signal(SIGHUP, sighandler);
+  signal(SIGUSR1, sighandler);
+}
+
+void pinitd(void)
+{
+  int pid;
+  int i;
+  if (pid = fork())
+  {
+    exit(0);
+  }
+  else if (pid < 0)
+  {
+    exit(1);
+  }
+
+  setsid();
+
+  if (pid = fork())
+  {
+    exit(0);
+  }
+  else if (pid < 0)
+  {
+    exit(1);
+  }
+
+  for (i = 0; i < NOFILE; ++i)
+  {
+    close(i);
+  }
+
+  chdir("/tmp");
+  umask(0);
+  return;
+}
+
+#endif
--- main.cpp
+++ main.cpp
@@ -9,6 +9,10 @@ int main(int argc, char** argv)
   if (!fsutils::is_dir_exists(wwwroot))
     return ENOENT;
 
+#if !defined(_WIN32) // runas daemon at linux platform.
+  extern void sinitd(void);
+  sinitd();
+#endif
   ftp_server server(wwwroot);
 
   server.run();

10e05ce34befa07484d203e7ff25d929fc2e7c4b
halx99 <halx99@live.com>
2019-11-20 22:13:43 +0800

    update

--- ftp_session.cpp
+++ ftp_session.cpp
@@ -180,6 +180,7 @@ void ftp_session::say_hello()
         {
           if (thandle_ctl_)
           {
+	    YASIO_LOG("the session: %p is expired!", thandle_ctl_);
             __service.close(thandle_ctl_);
             thandle_ctl_ = nullptr;
           }

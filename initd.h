f1fa10708506fc81f307647f47cb6c2d1091bcd6
halx99 <halx99@live.com>
2023-06-16 12:15:28 +0800

    Fix builds

--- initd.h
+++ initd.h
@@ -1,7 +1,7 @@
 #if !defined(_WIN32)
 #  if defined(__APPLE__)
 #    include <TargetConditionals.h>
-#    if TARGET_OS_MAC
+#    if TARGET_OS_OSX
 #      define _HAVE_INITD 1
 #    endif
 #  elif defined(__linux__) && !defined(__ANDROID__)
